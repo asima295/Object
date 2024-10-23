31cf69230e0bffb3d7c9148a4121e2ae11815ad3
asima295 <ftasleem581@gmail.com>
2024-10-23 09:12:43 +0500

    Add files via upload

---
+++ object.js
@@ -0,0 +1,46 @@
+const book = {
+  firstName:"jhon",
+  lastName:"doe",
+   number:500,  
+  };
+book.color = "black";
+book["name"] ;
+console.log(book);
+
+
+
+
+const car = {
+    type:"Fiat",
+     model:"500",
+      color:"white"
+    };
+car.color = "red";
+car ["Johnson"];
+console.log(car);
+
+
+
+
+const veg = {
+  name:"cabbage",
+   RS:"500",
+    color:"green"
+  };
+book. name = "cabbage";
+car.color = "green";
+document.getElementById("d1").innerHTML = " " + veg.name; 
+
+
+
+
+const fruit = {
+  name:"apple",
+  Rs:500,
+ color:"red"
+  };
+fruit.name="orange";
+fruit["color"]
+console.log(fruit);
+
+
